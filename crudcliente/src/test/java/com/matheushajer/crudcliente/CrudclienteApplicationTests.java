7d2fd09246c1dfa5e0370b365d7d16791f0d73cd
matheushajer <matheushajer@gmail.com>
2021-05-17 14:16:32 -0300

    Project created

---
+++ crudcliente/src/test/java/com/matheushajer/crudcliente/CrudclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheushajer.crudcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
